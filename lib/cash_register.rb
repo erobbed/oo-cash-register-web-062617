c4568a29545a5744798bbe2610aa1a93da9b38e0
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-29 09:40:11 -0400

    added CashRegister class and methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+  attr_reader :items
+
+  def initialize(discount= 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity= 1)
+    @total += price * quantity
+    quantity.times {@items << title}
+  end
+
+  def apply_discount
+    if discount > 0
+      @total -= total * (discount/100.00)
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= @total
+
+  end
+end
